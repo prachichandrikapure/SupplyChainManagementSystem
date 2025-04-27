7ba2d3f6c97aa0eb56b9af10f2b52cd5ebd056de
Prachichandrikapure <prachichandrikapure36@gmail.com>
2025-04-28 00:43:00 +0530

    Add initial project files for Supply Chain Management System

---
+++ NotificationService/src/test/java/com/supplychain/NotificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplychain.NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ WarehouseService/src/test/java/com/supplychain/WarehouseService/WarehouseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplychain.WarehouseService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
